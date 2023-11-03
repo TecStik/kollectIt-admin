59a6929b6e9be2b682c75833ef965582672a8477
Syed Muhemin Ali <149074986+muhemin123@users.noreply.github.com>
2023-11-03 04:48:40 -0700

    className Bug solve
    className Bug solve

--- src/Component/Members/AllMembersList/MembersList.jsx
+++ src/Component/Members/AllMembersList/MembersList.jsx
@@ -71,39 +71,39 @@ export default function MembarList() {
 
     return (
 
-        <div class="card card-cascade narrower">
+        <div className="card card-cascade narrower">
             <div
-                class="view view-cascade gradient-card-header blue-gradient narrower py-2 mx-4 mb-3 d-flex justify-content-between align-items-center">
+                className="view view-cascade gradient-card-header blue-gradient narrower py-2 mx-4 mb-3 d-flex justify-content-between align-items-center">
 
                 <div>
-                    <button type="button" class="btn btn-outline-white btn-rounded btn-sm px-2">
-                        <i class="fas fa-th-large mt-0"></i>
+                    <button type="button" className="btn btn-outline-white btn-rounded btn-sm px-2">
+                        <i className="fas fa-th-large mt-0"></i>
                     </button>
-                    <button type="button" class="btn btn-outline-white btn-rounded btn-sm px-2">
-                        <i class="fas fa-columns mt-0"></i>
+                    <button type="button" className="btn btn-outline-white btn-rounded btn-sm px-2">
+                        <i className="fas fa-columns mt-0"></i>
                     </button>
                 </div>
 
-                <a href="" class="white-text mx-3">Allow Access</a>
+                <a href="" className="white-text mx-3">Allow Access</a>
 
                 <div>
-                    <button type="button" class="btn btn-outline-white btn-rounded btn-sm px-2">
-                        <i class="fas fa-pencil-alt mt-0"></i>
+                    <button type="button" className="btn btn-outline-white btn-rounded btn-sm px-2">
+                        <i className="fas fa-pencil-alt mt-0"></i>
                     </button>
-                    <button type="button" class="btn btn-outline-white btn-rounded btn-sm px-2">
-                        <i class="far fa-trash-alt mt-0"></i>
+                    <button type="button" className="btn btn-outline-white btn-rounded btn-sm px-2">
+                        <i className="far fa-trash-alt mt-0"></i>
                     </button>
-                    <button type="button" class="btn btn-outline-white btn-rounded btn-sm px-2">
-                        <i class="fas fa-info-circle mt-0"></i>
+                    <button type="button" className="btn btn-outline-white btn-rounded btn-sm px-2">
+                        <i className="fas fa-info-circle mt-0"></i>
                     </button>
                 </div>
 
             </div>
-            <div class="container mt-3">
+            <div className="container mt-3">
                 <h2>All Data Member List</h2>
 
-                <table class="table table-hover">
-                    <thead class="bg-light">
+                <table className="table table-hover">
+                    <thead className="bg-light">
                         <tr>
                             <th>Name</th>
                             <th>Email</th>
@@ -122,19 +122,19 @@ export default function MembarList() {
                                     <td>{v.employeeEmail}</td>
                                     <td>{v.employeePassword}</td>
                                     <td>
-                                        <span class="badge badge-warning rounded-pill d-inline">{v.Role}</span>
+                                        <span className="badge badge-warning rounded-pill d-inline">{v.Role}</span>
                                     </td>
                                     <td>Senior</td>
                                     <td>
                                         {Role === "SuperAdmin" ? <>
-                                            <button type="button" class="btn btn-success btn-rounded" onClick={() => Admin(v._id)}>Admin</button>
-                                            <button type="button" class="btn btn-warning btn-rounded" onClick={() => Cashier(v._id)}>Cashier</button>
-                                            <button type="button" class="btn btn-primary btn-rounded" onClick={() => Rider(v._id)}>Rider</button>
+                                            <button type="button" className="btn btn-success btn-rounded" onClick={() => Admin(v._id)}>Admin</button>
+                                            <button type="button" className="btn btn-warning btn-rounded" onClick={() => Cashier(v._id)}>Cashier</button>
+                                            <button type="button" className="btn btn-primary btn-rounded" onClick={() => Rider(v._id)}>Rider</button>
                                         </> : Role === "Admin" ? <>
-                                            <button type="button" class="btn btn-warning btn-rounded" onClick={() => Cashier(v._id)}>Cashier</button>
-                                            <button type="button" class="btn btn-primary btn-rounded" onClick={() => Rider(v._id)}>Rider</button>
+                                            <button type="button" className="btn btn-warning btn-rounded" onClick={() => Cashier(v._id)}>Cashier</button>
+                                            <button type="button" className="btn btn-primary btn-rounded" onClick={() => Rider(v._id)}>Rider</button>
                                         </> : Role === "Cashier" ? <>
-                                            <button type="button" class="btn btn-primary btn-rounded" onClick={() => Rider(v._id)}>Rider</button>
+                                            <button type="button" className="btn btn-primary btn-rounded" onClick={() => Rider(v._id)}>Rider</button>
                                         </> : <></>}
                                     </td>
                                 </tr>
