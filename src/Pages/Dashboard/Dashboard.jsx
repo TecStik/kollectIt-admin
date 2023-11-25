58cc55d27d029b0115da49deaac578ab0b995ae1
Syed Muhemin Ali <149074986+muhemin123@users.noreply.github.com>
2023-11-24 23:15:04 -0800

    updated
    updated

--- src/Pages/Dashboard/Dashboard.jsx
+++ src/Pages/Dashboard/Dashboard.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useContext } from "react";
+import React, { useState, useEffect, useContext } from "react"
 import "antd/dist/antd.css";
 import "./Dashboard.css";
 import PaymentConfirmation1 from '../../Component/PaymentConfirmation/ThankyouPayment';
